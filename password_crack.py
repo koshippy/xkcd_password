f3e0c339961950e1ddf2735cbbece1f44ab810c6
koshippy <kos.hippy@gmail.com>
2015-02-17 04:15:17 -0700

    Update password_crack.py
    Added commenting to the functions, and a return value to hash_wordlist

--- password_crack.py
+++ password_crack.py
@@ -3,7 +3,7 @@ import random,hashlib,itertools,time
 def load_wordlist(d_location=r'D:\dictionary_words.txt',number_of_words=2048):
     #This returns a list of 2048 words from a supplied file.
     #Note, the words must contain ONLY letters or it will skip it and try to find another one.
-    #If your dictionary does not have at least 2048
+    #If your dictionary does not have at least 2048 words, it will get stuck in a loop
     with open(d_location) as d_file:
         dict = d_file.read()
     dict = dict.split('\n')
@@ -16,6 +16,10 @@ def load_wordlist(d_location=r'D:\dictionary_words.txt',number_of_words=2048):
     return wordlist
 
 def hash_wordlist(words,length,average=False):
+    #If average is False, this will hash every single combination and return the total time
+    #If average is True, will perform 100,000,000 hashes and return the average time for 10,000,000 hashes.
+    #Does SHA-256 hash of your wordlist. Words and length of password is adjustable.
+    #Its probably possible to use this to test a brute-force approach if your "wordlist" is actually a list of characters, however length will be fixed.
     progress = 0
     hash_times,averages = [time.time()],[]
     print('hashing')
@@ -25,10 +29,11 @@ def hash_wordlist(words,length,average=False):
         progress += 1
         if not progress%10000000:
             hash_times.append(time.time())
-            averages.append(hash_times[-1]-hash_times[0])
+            averages.append(hash_times[-1]-hash_times[-2])
             print('%s\nHash: %s\nTime: %s\n' % (password,hash,averages[-1]))
             if average and len(averages) == 10: return sum(averages)/10
-            if not average: averages = []
+            if not average: averages = []#This is just so averages does not become a large block of memory if you dont plan on averaging.
+    return hash_times[-1]-hash_times[0]
 
 def crack():
     wordlist = load_wordlist()
